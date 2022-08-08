8a2209cbd68a476b835544603e167e91a07cbacb
Antonin Folliasson <antonin@aitio.co>
2022-08-08 08:13:36 +0200

    initial functions setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState } from "react"
+
 function App() {
+
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const doubleNumber = slowFunction(number)
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
+  }
+
   return (
     <div className="App">
       <h1>My Full Page</h1>
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </div>
   )
 
+}
+
+function slowFunction(num) {
+  console.log('calling Slow Function')
+  for (let i = 0; i < 1000000000*0.2; i++) {}
+  return num + 2
 }
 
 export default App
